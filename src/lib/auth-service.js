c8e889117bd3f938a0094eae9b8395fc8f67c38d
melkorCba <BashikaDS@compello.com>
2022-07-29 11:08:38 +0530

    add js docs commnets

--- src/lib/auth-service.js
+++ src/lib/auth-service.js
@@ -1,11 +1,27 @@
 const keys = null;
 const { OAuth2Client } = require("google-auth-library");
 const utills = require("./utills")();
+
+/**
+ * Get Auth2Client that can be used to genereate new Auth Client
+ * @returns {{oAuth2ClientTypes: Object, getAuth:Function}} client types and getAuth function
+ */
+
 const Auth2Client = (() => {
+  /**
+   * Client types that is supported by getAuth
+   * @param {{QUICK: number, COMPLETE:String}} oAuth2ClientTypes description
+   */
   const oAuth2ClientTypes = Object.freeze({
     QUICK: "QUICK",
     COMPLETE: "COMPLETE",
   });
+  /**
+   * Get Auth client with requested type
+   * @param {{token:String}} data  - Access token object
+   * @param {oAuth2ClientTypes} scopes  - client type (Quick or Complete)
+   * @returns {OAuth2Client} AuthClient Object
+   */
   const getAuth = ({ data, type }) => {
     if (type === oAuth2ClientTypes.QUICK) {
       return {
@@ -29,14 +45,21 @@ const Auth2Client = (() => {
   };
 })();
 
-const getAuthData = async (scopes) => {
+/**
+ * Get Auth data required to request a token for given scopes
+ * @param {OAuth2Client} client  - OAuth2Client instance
+ * @param {string[]} scopes  - required scopes
+ * @returns {{authUrl:String, codes:Obejct}} authUrl and PKCE codes
+ */
+
+const getAuthData = async (client, scopes) => {
   try {
     // Generate a code_verifier and code_challenge
-    const codes = await oAuth2Client.generateCodeVerifierAsync();
+    const codes = await client.generateCodeVerifierAsync();
 
     // Generate the url that will be used for the consent dialog.
     return {
-      authUrl: oAuth2Client.generateAuthUrl({
+      authUrl: client.generateAuthUrl({
         access_type: "offline",
         scope: scopes,
         // When using `generateCodeVerifier`, make sure to use code_challenge_method 'S256'.
@@ -51,13 +74,23 @@ const getAuthData = async (scopes) => {
   }
 };
 
-const getToken = async (code, codeVerifier) => {
+/**
+ * Acquire access tokens from Auth code
+ * Also set access tokens in for provided client internally
+ * @param {OAuth2Client} client  - OAuth2Client instance
+ * @param {string} code  - Auth code
+ * @param {string} codeVerifier
+ * @returns {string} Access token
+ */
+
+const getToken = async (client, code, codeVerifier) => {
   try {
-    const response = await oAuth2Client.getToken({
+    const response = await client.getToken({
       code,
       codeVerifier: codeVerifier,
     });
-    return response.tokens["access_token"];
+    client.setCredentials(response.tokens);
+    return response.tokens?.access_token;
   } catch (err) {
     console.log(err);
   }
