4990a1378c232cdb0204ce23c85bf11523dd8452
melkorCba <BashikaDS@compello.com>
2022-08-01 16:51:56 +0530

    support submoule imports

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 module.exports = {
     utills: require('./lib/utills')(),
     clients: require('./lib/clients'),
-    authService:  require('./lib/auth-service'),
+    authService:  require('./lib/auth-scopes'),
     docsService: require('./lib/docs-service'),
     driveService: require('./lib/drive-service'),
     
