325019d35894264da809e001eb1090b7b752a8d1
melkorCba <BashikaDS@compello.com>
2022-08-01 21:30:03 +0530

    set auth-scopes as subpath

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 module.exports = {
     utills: require('./lib/utills')(),
     clients: require('./lib/clients'),
-    authService:  require('./lib/auth-scopes'),
     docsService: require('./lib/docs-service'),
     driveService: require('./lib/drive-service'),
     
